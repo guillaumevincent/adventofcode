26c90b3e9c217cd45966d6a57d23f4e94a00dea9
Guillaume Vincent <gvincent@redhat.com>
2021-12-01 10:57:32 +0100

    01a

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -1,2 +1,32 @@
 def sum(value1, value2):
     return value1 + value2
+
+
+# https://docs.python.org/3/library/typing.html
+Measurements = list[str]
+
+
+def number_of_times_a_depth_measurement_increases(measurements: Measurements):
+    if len(measurements) == 0:
+        return 0
+
+    # list comprehension
+    measurements = [int(measurement) for measurement in measurements]
+    # ms = []
+    # for measurement in measurements:
+    #     ms.append(int(measurement))
+    # measurements = ms
+
+    current_value = measurements[0]
+    nb_of_increases = 0
+    for measurement in measurements:
+        if measurement > current_value:
+            nb_of_increases += 1
+        current_value = measurement
+    return nb_of_increases
+
+
+if __name__ == "__main__":
+    measurements = open("in.txt").readlines()
+    increases = number_of_times_a_depth_measurement_increases(measurements)
+    print(f"Number of increases {increases}")
--- 2021/01/test.py
+++ 2021/01/test.py
@@ -2,11 +2,66 @@
 
 import unittest
 from main import sum
+from main import number_of_times_a_depth_measurement_increases
 
 
 class TestsMain(unittest.TestCase):
     def test_sum(self):
         assert sum(1, 1) == 2
+
+    def test_count_number_of_increases_with_empty_mesurements(self):
+        # AAA
+        # Arrange
+        measurements = []
+        # Act
+        increases = number_of_times_a_depth_measurement_increases(measurements)
+        # Assert
+        self.assertEquals(increases, 0)
+
+    def test_count_number_of_increases_with_one_measurement(self):
+        # AAA
+        # Arrange
+        measurements = [
+            "199",
+        ]
+        # Act
+        increases = number_of_times_a_depth_measurement_increases(measurements)
+        # Assert
+        self.assertEquals(increases, 0)
+
+    def test_count_number_of_increases(self):
+        # AAA
+        # Arrange
+        measurements = [
+            "199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263",
+        ]
+        # Act
+        increases = number_of_times_a_depth_measurement_increases(measurements)
+        # Assert
+        self.assertEquals(increases, 7)
+
+    def test_number_of_times_a_depth_measurement_increases_convert_measurements_to_numbers(
+        self,
+    ):
+        # AAA
+        # Arrange
+        measurements = [
+            "100",
+            "20",
+        ]
+        # Act
+        increases = number_of_times_a_depth_measurement_increases(measurements)
+        # Assert
+        self.assertEquals(increases, 0)
 
 
 if __name__ == "__main__":
